443e03fa922724f6c7650d01b265fe339891fc6b
Irene Hita <irene.hita7@gmail.com>
2020-10-26 19:39:39 +0100

    cuarta subida creo

--- p-tecnica/src/app/app-routing.module.ts
+++ p-tecnica/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InfoComponent } from './pages/info/info.component';
-import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "login", pathMatch: "full"},
-  {path: "login", component: LoginComponent},
+  {path: "", redirectTo: "register", pathMatch: "full"},
   {path: "register", component: RegisterComponent},
   {path: "profile", component: ProfileComponent},
   {path: "info", component: InfoComponent}
--- p-tecnica/src/app/app.module.ts
+++ p-tecnica/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { InfoComponent } from './pages/info/info.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
@@ -13,7 +12,6 @@ import { ProfileComponent } from './pages/profile/profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     InfoComponent,
     RegisterComponent,
     ProfileComponent
